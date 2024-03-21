e5b7e2dbcc1269dd8e744fb9ed6cfe6fdf6b1db0
gabi alvarenga <gabriel.dsalvarenga@gmail.com>
2024-03-21 16:29:24 -0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
     $db_name = "movie-star";
     $db_host = "localhost";
     $db_user = "root";
-    $db_pass = "Magikarp2.";
+    $db_pass = "";
 
     $conn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 
